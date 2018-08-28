7b5d09f29197e0eceed53de8cb3fdbc5f28e013b
tanmay jain <tanmayjain.cs@gmail.com>
2018-08-28 16:36:22 +0530

    Update is_pangram.py

--- IsPangram/is_pangram.py
+++ IsPangram/is_pangram.py
@@ -4,9 +4,25 @@ print('pangram' if len(letter_set) == 26 else 'not pangram')
 
 
 #--------------------------------------------------------------------------------------------------------------------------
+
 from collections import Counter
 s = input().lower()
 s_char_set = Counter(s)
 s_char_set.pop(' ', None)
 
 print('pangram' if len(s_char_set) == 26 else 'not pangram')
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+
+from collections import defaultdict
+
+s = input().lower()
+char_dict = defaultdict(lambda:0)
+
+for char in s:
+    char_dict[char] += 1
+
+char_dict.pop(' ',None)
+
+print('pangram' if len(char_dict) == 26 else 'not pangram')
