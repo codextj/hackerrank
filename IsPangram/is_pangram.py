0906bd4a4a75f2eb1ec54cc5f1124b105246a6b3
tanmay jain <tanmayjain.cs@gmail.com>
2018-08-28 16:28:58 +0530

    Update is_pangram.py

--- IsPangram/is_pangram.py
+++ IsPangram/is_pangram.py
@@ -1,3 +1,12 @@
 s = input().replace(' ','').lower() # input-constraints {a-z, A-Z, space}
 letter_set = set(s)
 print('pangram' if len(letter_set) == 26 else 'not pangram')
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+from collections import Counter
+s = input().lower()
+s_char_set = Counter(s)
+s_char_set.pop(' ', None)
+
+print('pangram' if len(s_char_set) == 26 else 'not pangram')
